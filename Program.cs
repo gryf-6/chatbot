0109464622d268d199057c06709bcc89e3cbe3dc
Adam <adamakaramanski06@gmail.com>
2024-03-30 13:13:46 +0000

    help im begging

--- Program.cs
+++ Program.cs
@@ -6,9 +6,24 @@ namespace chatbot_project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("welcome to legend park chatbot how may i help you today");
+            Console.WriteLine("our most frequently asked questions");
+            Console.WriteLine("1) rides and attractions");
+            Console.WriteLine("2) refreshments info");
+            
+            while (true)
+            {
+            string choice = console.writeline();
+            choice = choice.tolower();
+
+            switch (choice)
+            }
         }
+        
+      
+        
+    
+    
     }
 }
 
-//im gonna kms
